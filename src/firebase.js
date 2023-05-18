ff1bedb7280b75e277f08f0f49723d4b077416af
Iskanderhableni <89342940+Iskanderhableni@users.noreply.github.com>
2023-05-18 12:01:37 +0100

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/database"
 import "firebase/compat/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpvLaJPbjYLBuOU2o4KfkxrbFu1rQyYbI",
-  authDomain: "netflix-proj-19491.firebaseapp.com",
-  projectId: "netflix-proj-19491",
-  storageBucket: "netflix-proj-19491.appspot.com",
-  messagingSenderId: "799717216534",
-  appId: "1:799717216534:web:1186d2d4cb8200512d4037"
+  apiKey: "API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STOREGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
